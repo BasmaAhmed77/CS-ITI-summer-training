6a3460ec9fc5e2fad3f40ba079483c04fbc48653
BasmaAhmed77 <bm2597692@gmail.com>
2025-09-12 22:29:31 +0300

    updated day2.cs

--- C# day2/day2.cs
+++ C# day2/day2.cs
@@ -21,11 +21,34 @@ class Program
         {
             Console.WriteLine($"The name of student {i+1}: {students[i]}");
         }
-        
-        #endregion
 
-
-
+        #endregion student age for each track
+        Console.Write("Enter number of students: ");
+        int studentsNum = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number of tracks: ");
+        int tracksNum = Convert.ToInt32(Console.ReadLine());
+        int[,] ages = new int[tracksNum,studentsNum];
+        int[] sum = new int[studentsNum];
+        for (int i=0; i<tracksNum; i++)
+        {
+            for(int j = 0; j < studentsNum; j++)
+            {
+                Console.Write($"Enter age of student {j+1} in track {i+1}: ");
+                ages[i,j] = Convert.ToInt32(Console.ReadLine());
+                sum[i] += ages[i,j];
+            }
+        }
+        for (int i = 0; i < tracksNum; i++)
+        {
+            for (int j = 0; j < studentsNum; j++)
+            {
+                Console.WriteLine($"Age of Student {j+1} in track {i+1}: {ages[i,j]}");
+            }
+        }
+        for (int i = 0; i < tracksNum; i++)
+        {
+            Console.WriteLine($"Avg age of Track {i + 1}: {sum[i]/studentsNum}");
+        }
         #region
 
 
@@ -34,7 +57,7 @@ class Program
 
         #region time format
 
-       
+
 
         #endregion
 
