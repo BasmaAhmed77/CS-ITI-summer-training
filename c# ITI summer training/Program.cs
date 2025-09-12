87ba4b57e1155c7a6d49a8bf9ea25cd4cd891174
BasmaAhmed77 <bm2597692@gmail.com>
2025-09-12 13:18:16 +0300

    Updated Program.cs

--- c# ITI summer training/Program.cs
+++ c# ITI summer training/Program.cs
@@ -1,6 +1,13 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+int x = 10;
+Console.WriteLine(x);
 
-
+int y = 20;
+Console.WriteLine(y);
 
 
 
