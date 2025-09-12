e349ae4ee809711e4d929076fd00c40c59dcc99f
BasmaAhmed77 <bm2597692@gmail.com>
2025-09-12 13:59:04 +0300

    Added day1 and day2 folders with code files

---
+++ c# ITI summer training/day1/day1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__ITI_summer_training.day1
+{
+    internal class day1
+    {
+    }
+}
