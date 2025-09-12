b5fdc00ddb32c822f5ca3f5cf793576ff60b1807
BasmaAhmed77 <bm2597692@gmail.com>
2025-09-12 21:55:44 +0300

    updated day2.cs

--- C# day2/day2.cs
+++ C# day2/day2.cs
@@ -1,5 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Input number of students and their names
+        Console.Write("Enter number of students: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        string[] students = new string[size];
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("Enter the name of student number "+(i+1) + ": ");
+            students[i] = Console.ReadLine();
+        }
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"The name of student {i+1}: {students[i]}");
+        }
+        
+        #endregion
+
+
+
+        #region
+
+
+        #endregion
+
+
+        #region time format
+
+       
+
+        #endregion
+
+    }
+}
